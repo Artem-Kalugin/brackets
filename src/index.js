f9dc95dd6f77c6cdc1087a315570a00548716d0b
Artem-Kalugin <Aharlo4@gmail.com>
2020-10-21 00:21:36 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let prev;
+    do{
+    prev = str;
+    bracketsConfig.forEach(element => { 
+      str = str.replace(new RegExp("\\" + element[0]+"\\" +element[1], "g"), '');
+      str = str.replace(new RegExp(element[0]+element[1], "g"), '');
+    });
+    }while (str!=prev);
+    return str==="";
 }
